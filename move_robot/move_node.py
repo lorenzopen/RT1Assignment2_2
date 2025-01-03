346c6ac350d8bdd2b0936409633e59e6427e0e95
lorenzopen <lorenzopen@github.com>
2025-01-03 20:58:04 +0000

    best pract change node

--- move_robot/move_node.py
+++ move_robot/move_node.py
@@ -1,27 +1,49 @@
 import rclpy
 from rclpy.node import Node
 from geometry_msgs.msg import Twist
+import time
+
+class MoveRobotNode(Node):
+    def __init__(self):
+        super().__init__('move_robot')
+        self.pub = self.create_publisher(Twist, '/cmd_vel', 10)
+
+        timer_period = 1
+        self.timer = self.create_timer(timer_period, self.timer_callback)
+        
+        self.get_velocity()
+
+        
+
+
+    def get_velocity(self):
+        try:
+            while rclpy.ok():
+                user_input = input("Enter 'linear angular' to set velocities: ")
+                try:
+                    self.linear, self.angular = map(float, user_input.split())
+                    break
+                except ValueError:
+                    self.get_logger().error("Invalid input. Please enter two numbers separated by a space.")
+        except KeyboardInterrupt:
+            pass
+
+    def timer_callback(self):
+        msg = Twist()
+        msg.linear.x = self.linear
+        msg.angular.z = self.angular
+        self.pub.publish(msg)
+
+        time.sleep(1.0)  # 1 second
+        msg.linear.x = 0.0
+        msg.angular.z = 0.0
+        self.pub.publish(msg)
+        self.get_velocity()
 
 def main(args=None):
     rclpy.init(args=args)
-    node = rclpy.create_node('move_robot')
-    pub = node.create_publisher(Twist, '/cmd_vel', 10)
-    node.get_logger().info('MoveRobotNode has been started.')
-
-    def move_robot(linear, angular):
-        msg = Twist()
-        msg.linear.x = linear
-        msg.angular.z = angular
-        pub.publish(msg)
-        node.get_logger().info(f'Published linear={linear}, angular={angular}')
-
-    try:
-        while rclpy.ok():
-            linear = float(input('Enter linear velocity: '))
-            angular = float(input('Enter angular velocity: '))
-            move_robot(linear, angular)
-    except KeyboardInterrupt:
-        pass
+    vel_pub = MoveRobotNode()
+    rclpy.spin(vel_pub)
 
     node.destroy_node()
     rclpy.shutdown()
