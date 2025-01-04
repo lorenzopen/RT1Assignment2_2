bb2cc6e0c85111b147acc5ea6c6f452d1faf8ce3
lorenzopen <lorenzopen@github.com>
2025-01-04 16:05:59 +0000

    sub odom and boundary

--- move_robot/move_node.py
+++ move_robot/move_node.py
@@ -1,52 +1,75 @@
 import rclpy
 from rclpy.node import Node
 from geometry_msgs.msg import Twist
+from nav_msgs.msg import Odometry
 import time
 
 class MoveRobotNode(Node):
     def __init__(self):
         super().__init__('move_robot')
-        self.pub = self.create_publisher(Twist, '/cmd_vel', 10)
+        self.sub_odom = self.create_subscription(Odometry, '/odom', self.odom_callback, 10)
+        self.pub_vel = self.create_publisher(Twist, '/cmd_vel', 10)
 
-        timer_period = 1
-        self.timer = self.create_timer(timer_period, self.timer_callback)
-        
+        self.linear = 0.0
+        self.last_linear = 0.0
+        self.angular = 0.0
+        self.last_angular = 0.0
+                
         self.get_velocity()
 
-        
-
-
     def get_velocity(self):
-        try:
-            while rclpy.ok():
+        while rclpy.ok():
+            try:
                 user_input = input("Enter 'linear angular' to set velocities: ")
-                try:
-                    self.linear, self.angular = map(float, user_input.split())
-                    break
-                except ValueError:
-                    self.get_logger().error("Invalid input. Please enter two numbers separated by a space.")
-        except KeyboardInterrupt:
-            pass
+                self.linear, self.angular = map(float, user_input.split())
+                self.move_robot()
+            except ValueError:
+                self.get_logger().error("Invalid input. Please enter two numbers separated by a space.")
 
-    def timer_callback(self):
+
+    def odom_callback(self, msg):
+        if (msg.pose.pose.position.x > 9.0 or msg.pose.pose.position.x < 1.0 or msg.pose.pose.position.y < 1.0 or msg.pose.pose.position.y > 9.0):
+            self.get_logger().warn("Boundary reached! Stopping and reversing.")
+            self.stop()
+            self.reverse_robot()
+
+    def move_robot(self):
         msg = Twist()
         msg.linear.x = self.linear
         msg.angular.z = self.angular
-        self.pub.publish(msg)
+        self.pub_vel.publish(msg)
 
-        time.sleep(1.0)  # 1 second
+        self.last_linear = self.linear
+        self.last_angular = self.angular
+
+        time.sleep(1.0)
+        self.stop()
+
+
+    def stop(self):
+        msg = Twist()
         msg.linear.x = 0.0
         msg.angular.z = 0.0
-        self.pub.publish(msg)
-        self.get_velocity()
+        self.pub_vel.publish(msg)
+        
+    def reverse_robot(self):
+        msg = Twist()
+        msg.linear.x = 0.2 * -self.last_linear
+        msg.angular.z = 0.2 * -self.last_angular
+        self.pub_vel.publish(msg)
+
+        time.sleep(1.0)
+        self.stop()
+
 
 def main(args=None):
     rclpy.init(args=args)
     vel_pub = MoveRobotNode()
-    rclpy.spin(vel_pub)
-
-    node.destroy_node()
-    rclpy.shutdown()
+    try:
+        rclpy.spin(vel_pub)
+    except KeyboardInterrupt:
+        vel_pub.destroy_node()
+        rclpy.shutdown()
 
 if __name__ == '__main__':
     main()
