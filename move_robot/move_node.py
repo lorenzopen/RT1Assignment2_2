5e02c183309424a392590c32e04e31f664d1671f
lorenzopen <lorenzopen@github.com>
2025-01-05 11:03:27 +0000

    fix

--- move_robot/move_node.py
+++ move_robot/move_node.py
@@ -7,6 +7,7 @@ import time
 class MoveRobotNode(Node):
     def __init__(self):
         super().__init__('move_robot')
+
         self.sub_odom = self.create_subscription(Odometry, '/odom', self.odom_callback, 10)
         self.pub_vel = self.create_publisher(Twist, '/cmd_vel', 10)
 
@@ -14,24 +15,27 @@ class MoveRobotNode(Node):
         self.last_linear = 0.0
         self.angular = 0.0
         self.last_angular = 0.0
+        self.msg_odom = None
                 
-        self.get_velocity()
+        self.control_timer = self.create_timer(1, self.control_loop)  # Ogni 0.1 secondi
+        #self.get_velocity()
 
     def get_velocity(self):
-        while rclpy.ok():
-            try:
-                user_input = input("Enter 'linear angular' to set velocities: ")
-                self.linear, self.angular = map(float, user_input.split())
-                self.move_robot()
-            except ValueError:
-                self.get_logger().error("Invalid input. Please enter two numbers separated by a space.")
-
+        
+        try:
+            user_input = input("Enter 'linear angular' to set velocities: ")
+            self.linear, self.angular = map(float, user_input.split())
+            self.move_robot()
+        except ValueError:
+            self.get_logger().error("Invalid input. Please enter two numbers separated by a space.")
+        except KeyboardInterrupt:
+            self.get_logger().info("Exiting velocity input.")
+        
 
     def odom_callback(self, msg):
-        if (msg.pose.pose.position.x > 9.0 or msg.pose.pose.position.x < 1.0 or msg.pose.pose.position.y < 1.0 or msg.pose.pose.position.y > 9.0):
-            self.get_logger().warn("Boundary reached! Stopping and reversing.")
-            self.stop()
-            self.reverse_robot()
+        self.msg_odom = msg
+        x = msg.pose.pose.position.x
+        y = msg.pose.pose.position.y
 
     def move_robot(self):
         msg = Twist()
@@ -45,13 +49,24 @@ class MoveRobotNode(Node):
         time.sleep(1.0)
         self.stop()
 
-
     def stop(self):
         msg = Twist()
         msg.linear.x = 0.0
         msg.angular.z = 0.0
         self.pub_vel.publish(msg)
         
+    def control_loop(self):
+        if self.msg_odom is not None:
+            x, y = self.msg_odom.pose.pose.position.x, self.msg_odom.pose.pose.position.y
+            if x > 9.0 or x < -9.0 or y > 9.0 or y < -9.0:
+                self.stop()
+                self.reverse_robot()
+            else:
+                self.get_velocity()
+            
+        else:
+            self.get_logger().warn("No odometry data received yet.")
+
     def reverse_robot(self):
         msg = Twist()
         msg.linear.x = 0.2 * -self.last_linear
@@ -68,6 +83,8 @@ def main(args=None):
     try:
         rclpy.spin(vel_pub)
     except KeyboardInterrupt:
+        pass
+    finally:
         vel_pub.destroy_node()
         rclpy.shutdown()
 
