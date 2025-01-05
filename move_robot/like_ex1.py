5e2ab79ec8346adce2019d128ed973919813ebdb
lorenzopen <lorenzopen@github.com>
2025-01-05 18:40:20 +0000

    implement auto movement in boundaries

--- move_robot/like_ex1.py
+++ move_robot/like_ex1.py
@@ -7,9 +7,28 @@ class PositionBasedMotion(Node):
 
     def __init__(self):
         super().__init__('ex1_like')
-        self.publisher_ = self.create_publisher(Twist, '/cmd_vel', 10)
-        self.subscription_ = self.create_subscription(Odometry,'/odom',self.odometry_listener_callback,10)
+        self.pub = self.create_publisher(Twist, '/cmd_vel', 10)
+        self.sub = self.create_subscription(Odometry,'/odom',self.odomcallback,10)
 
+    def odomcallback(self, msg):
+        position = msg.pose.pose.position
+        orientation = msg.pose.pose.orientation
+
+        self.get_logger().info(f"Position: x={position.x:.2f}, y={position.y:.2f}, z={position.z:.2f}")
+
+        vel_msg = Twist()
+
+        if position.x > 9.0: 
+            vel_msg.linear.x = 0.5  
+            vel_msg.angular.z = 0.5   
+        elif position.x < 1.0:
+            vel_msg.linear.x = 0.5
+            vel_msg.angular.z = 0.5
+        else:
+            vel_msg.linear.x = 0.5 
+            vel_msg.angular.z = 0.0
+
+        self.pub.publish(vel_msg)
 
 def main(args=None):
     rclpy.init(args=args)
